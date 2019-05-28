9de80882bae9aba93dd345e01dac65c805ad5d7a
rybo713 <ryanwong713@gmail.com>
2019-05-28 06:52:30 -0700

    Updated Tools.sh to v0.3

--- Tools/tools.sh
+++ Tools/tools.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Bold / Non-bold
 BOLD="\033[1m"
@@ -10,19 +10,24 @@ OFF="\033[m"
 
 gatekeeper()
 {
-	echo "${GREEN}[3rd Party]${OFF}: Enabling ${BOLD}3rd Party${OFF} application support"
+	printf "${GREEN}[3rd Party]${OFF}: Enabling ${BOLD}3rd Party${OFF} application support\n"
 	sudo spctl --master-disable
+    printf "${GREEN}[DONE]${OFF}\n"
 }
 
 touchid()
 {
-	echo "${GREEN}[TouchID]${OFF}: disabling ${BOLD}TouchID${OFF} launch daemons"
+	printf "${GREEN}[TouchID]${OFF}: disabling ${BOLD}TouchID${OFF} launch daemons\n"
 	sudo rm /System/Library/LaunchDaemons/com.apple.biometrickitd.plist
 	sudo rm /System/Library/LaunchDaemons/com.apple.biokitaggdd.plist
+    printf "${GREEN}[DONE]${OFF}\n"
 }
 
+while true; do
+
+clear
 echo ""
-echo "Tools v0.1"
+printf "${BLUE}${BOLD}      Tools v0.3${OFF}\n"
 echo ""
 echo "1) Disable GateKeeper"
 echo "2) Disable TouchID"
@@ -31,10 +36,40 @@ echo ""
 read -p "> " output
 
 if [ $output = 1 ]; then
+    clear
     gatekeeper
+    echo ""
+    echo "(b) Back"
+    echo "(q) Exit"
+    echo ""
+    read -p "> " output1
+
+    if [ $output1 = "b" ]; then
+        clear
+    elif [ $output1 = "q" ]; then
+        clear
+        exit 0
+    fi
+
 elif [ $output = 2 ]; then
+    clear
     touchid
+    echo ""
+    echo "(b) Back"
+    echo "(q) Exit"
+    echo ""
+    read -p "> " output2
+
+    if [ $output2 = "b" ]; then
+        clear
+    elif [ $output2 = "q" ]; then
+        clear
+        exit 0
+    fi
+
 elif [ $output = 3 ]; then
+    clear
     exit 0
 fi
 
+done
